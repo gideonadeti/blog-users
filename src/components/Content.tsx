1c08497c36a901c046b281044264f986b39c47c1
gideonadeti <gideonadeti0@gmail.com>
2024-10-21 19:29:47 +0000

    Create axios instance for fetching data

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,7 +1,48 @@
+import { useEffect, useState } from "react";
+
+import axiosInstance from "../utils/axios-instance";
+
 export default function Content() {
-  return (
-    <div className="content container-fluid py-3 overflow-y-auto" style={{ maxHeight: "90vh" }}>
-      Content
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        startFetch();
+
+        const [postsResponse, tagsResponse] = await Promise.all([
+          axiosInstance.get("/posts"),
+          axiosInstance.get("/tags"),
+        ]);
+
+        console.table(postsResponse.data.posts);
+        console.table(tagsResponse.data.tags);
+      } catch (error) {
+        console.error(error);
+
+        setError("Something went wrong.");
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  function startFetch() {
+    setError("");
+  }
+
+  {
+    return (
+      <div
+        className="content container-fluid py-3 overflow-y-auto"
+        style={{ maxHeight: "90vh" }}
+      >
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </div>
+    );
+  }
 }
