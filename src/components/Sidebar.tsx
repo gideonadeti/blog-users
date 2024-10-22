e183245dc7b95eea25567ff3ffa6e583423c8ce0
gideonadeti <gideonadeti0@gmail.com>
2024-10-22 06:06:21 +0000

    Display tags in a select in sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,26 @@
+import Form from "react-bootstrap/Form";
+
+import { useTagsStore } from "../stores/tags";
+
 export default function Sidebar() {
+  const { tags } = useTagsStore();
+
   return (
     <div
       className="container-fluid position-absolute top-0 bottom-0 border-end d-flex flex-column justify-content-between py-3"
-      style={{ width: "240px" }}
+      style={{ width: "276px" }}
     >
-      Sidebar
+      <Form.Select
+        multiple
+        className="h-100"
+        style={{ scrollbarWidth: "thin" }}
+      >
+        {tags.map((tag) => (
+          <option key={tag.id} value={tag.name}>
+            {tag.name}
+          </option>
+        ))}
+      </Form.Select>
     </div>
   );
 }
